6bd3e65f704f57ba13b971aba3e0c8927f6ac835
Sai <sai4rall@gmail.com>
2025-03-16 17:33:40 -0400

    added proc call

--- src/main/java/com/example/client_onboarding/repository/ClientServiceRepository.java
+++ src/main/java/com/example/client_onboarding/repository/ClientServiceRepository.java
@@ -1,9 +1,14 @@
-//package com.example.client_onboarding.repository;
-//
-//import com.example.client_onboarding.dao.request.ClientService;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-////@Repository
-//public interface ClientServiceRepository extends JpaRepository<ClientService, Long> {
-//}
+package com.example.client_onboarding.repository;
+
+import com.example.client_onboarding.dao.request.ClientService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotEmpty;
+
+//@Repository
+public interface ClientServiceRepository extends JpaRepository<ClientService, Long> {
+    @Query("UPDATE ClientService c SET c.clientName = ?1, c.serviceEnrolled = ?2 WHERE c.uuid = ?3")
+    void updateClientService(@NotEmpty(message = "Client name must not be empty") String clientName, @NotEmpty(message = "Service enrolled must not be empty") String serviceEnrolled, String uuid);
+}
--- src/main/java/com/example/client_onboarding/service/ClientOnboardingService.java
+++ src/main/java/com/example/client_onboarding/service/ClientOnboardingService.java
@@ -1,16 +1,55 @@
 package com.example.client_onboarding.service;
 
 import com.example.client_onboarding.dao.request.ClientService;
+import com.example.client_onboarding.repository.ClientServiceRepository;
 import com.example.client_onboarding.util.AppUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
+
+import javax.sql.DataSource;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ClientOnboardingService {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    private SimpleJdbcCall simpleJdbcCall;
+
+    @Autowired
+    ClientServiceRepository clientServiceRepository;
+
+
+
+    public ClientOnboardingService(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcCall = new SimpleJdbcCall(dataSource)
+                .withProcedureName("your_stored_procedure_name");
+    }
+
     public ClientService onBoardClient(@Valid ClientService clientService) {
-      String uuid= AppUtils.generateUUID();
+        String uuid = AppUtils.generateUUID();
         System.out.printf("uuid: %s", uuid);
-return clientService;
+
+        clientServiceRepository.updateClientService(clientService.getClientName(), clientService.getServiceEnrolled(), uuid);
+
+
+        HashMap<String, String> procArgs = new HashMap<>();
+        procArgs.put("uuid", uuid);
+        procArgs.put("serviceName", clientService.getServiceEnrolled());
+        procArgs.put("clientName", clientService.getClientName());
+
+        callStoredProcedure(Map.of("uuid", uuid));
+        return clientService;
+    }
+
+
+    public Map<String, Object> callStoredProcedure(Map<String, Object> inParams) {
+        return simpleJdbcCall.execute(inParams);
     }
 }
